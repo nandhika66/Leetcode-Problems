a347f3a6bfac71d8549eeb172abe9f0fe239fb3f
Nandhika A <139376918+nandhika66@users.noreply.github.com>
2025-10-30 18:24:04 +0530

    Time: 5 ms (64.55%) | Memory: 44.3 MB (38.27%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int temp=x;
+        while(x>0){
+            rev=(rev*10)+(x%10);
+            x=x/10;
+        }
+        if(temp==rev){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
