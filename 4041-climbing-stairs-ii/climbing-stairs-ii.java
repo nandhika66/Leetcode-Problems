1a77d79c9700abd2b4ff6ca435f65f131b1e2eed
Nandhika A <139376918+nandhika66@users.noreply.github.com>
2025-12-09 10:44:44 +0530

    Time: 48 ms (23.49%) | Memory: 134.8 MB (5.81%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,20 +1,23 @@
-import java.util.*;
 class Solution {
+    public int solve(int n,int dp[],int costs[]){
+        if(n==0) return dp[0] = 0;
+        if(n==1) return dp[1] = costs[0]+1;
+        if(n==2) return dp[2] = Math.min(costs[1]+4, solve(1,dp,costs)+costs[1]+1);
+        if(n==3) return dp[3] = Math.min(
+            Math.min(costs[2]+9, solve(1,dp,costs)+costs[2]+4),
+            solve(2,dp,costs)+costs[2]+1);
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n] = Math.min(
+        Math.min((solve(n-1,dp,costs)+costs[n-1]+(n-(n-1))*(n-(n-1))),
+        (solve(n-2,dp,costs)+costs[n-1]+(n-(n-2))*(n-(n-2)))),
+        (solve(n-3,dp,costs)+costs[n-1]+(n-(n-3))*(n-(n-3)))
+        );
+    }
     public int climbStairs(int n, int[] costs) {
         int dp[] = new int[n+1];
-        dp[0] = 0;
-        for(int i=1;i<=n;i++){
-            dp[i] = Integer.MAX_VALUE;
-            if(i-1>=0){
-                dp[i]=Math.min(dp[i],dp[i-1]+costs[i-1]+(i-(i-1))*(i-(i-1)));
-            }
-            if(i-2>=0){
-                dp[i]=Math.min(dp[i],dp[i-2]+costs[i-1]+(i-(i-2))*(i-(i-2)));
-            }
-            if(i-3>=0){
-                dp[i]=Math.min(dp[i],dp[i-3]+costs[i-1]+(i-(i-3))*(i-(i-3)));
-            }
-        }
-        return dp[n];
+        Arrays.fill(dp,-1);
+        return solve(n,dp,costs);
     }
 }
