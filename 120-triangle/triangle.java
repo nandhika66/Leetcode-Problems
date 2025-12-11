b0316fc5bf94b92bff3e782588abdb00b688b55e
Nandhika A <139376918+nandhika66@users.noreply.github.com>
2025-12-11 12:35:34 +0530

    Time: 8 ms (22.59%) | Memory: 46.4 MB (13.13%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,11 +1,11 @@
 class Solution {
-    public int minimumTotal(List<List<Integer>> tri) {
-        for (int i = tri.size() - 2; i >= 0; i--)
-            for (int j = 0; j < tri.get(i).size(); j++)
-                tri.get(i).set(j, tri.get(i).get(j) + Math.min(
-                    tri.get(i + 1).get(j),
-                    tri.get(i + 1).get(j + 1)
-                ));
-        return tri.get(0).get(0);
+    public int minimumTotal(List<List<Integer>> triangle) {
+        //dp only but we are using the same arraylist itself
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=triangle.get(i).size()-1;j>=0;j--){
+                triangle.get(i).set(j,triangle.get(i).get(j) + Math.min(triangle.get(i+1).get(j),triangle.get(i+1).get(j+1)));
+            }
+        }
+        return triangle.get(0).get(0);
     }
 }
