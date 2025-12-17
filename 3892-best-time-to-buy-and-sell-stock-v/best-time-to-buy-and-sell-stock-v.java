a3b51af408545127078dd9007113591d285e4a4b
Nandhika A <139376918+nandhika66@users.noreply.github.com>
2025-12-17 20:46:34 +0530

    Time: 10 ms (99.29%) | Memory: 46.5 MB (86.17%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
@@ -1,46 +1,32 @@
 class Solution {
-
-    private int[] prices;
-    private long[][][] memo;
-
     public long maximumProfit(int[] prices, int k) {
-        this.prices = prices;
         int n = prices.length;
-        memo = new long[n][k + 1][3];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= k; j++) {
-                for (int s = 0; s < 3; s++) {
-                    memo[i][j][s] = -1;
-                }
+        if (n < 2 || k == 0) return 0;
+
+        long[] dp_prev = new long[n];
+        long[] dp_cur = new long[n];
+
+        for (int t = 1; t <= k; t++) {
+            long best_buy = -prices[0];
+            long best_short = prices[0];
+            dp_cur[0] = 0;
+
+            for (int i = 1; i < n; i++) {
+                long a = dp_cur[i - 1];
+                long b = best_buy + prices[i];
+                long c = best_short - prices[i];
+
+                dp_cur[i] = Math.max(Math.max(a, b), c);
+
+                best_buy = Math.max(best_buy, dp_prev[i - 1] - prices[i]);
+                best_short = Math.max(best_short, dp_prev[i - 1] + prices[i]);
             }
-        }
-        return dfs(n - 1, k, 0);
-    }
 
-    private long dfs(int i, int j, int state) {
-        if (j == 0) {
-            return 0;
-        }
-        if (i == 0) {
-            return state == 0 ? 0 : (state == 1 ? -prices[0] : prices[0]);
-        }
-        if (memo[i][j][state] != -1) {
-            return memo[i][j][state];
+            long[] temp = dp_prev;
+            dp_prev = dp_cur;
+            dp_cur = temp;
         }
 
-        int p = prices[i];
-        long res;
-        if (state == 0) {
-            res = Math.max(
-                dfs(i - 1, j, 0),
-                Math.max(dfs(i - 1, j, 1) + p, dfs(i - 1, j, 2) - p)
-            );
-        } else if (state == 1) {
-            res = Math.max(dfs(i - 1, j, 1), dfs(i - 1, j - 1, 0) - p);
-        } else {
-            res = Math.max(dfs(i - 1, j, 2), dfs(i - 1, j - 1, 0) + p);
-        }
-        memo[i][j][state] = res;
-        return res;
+        return dp_prev[n - 1];
     }
 }
